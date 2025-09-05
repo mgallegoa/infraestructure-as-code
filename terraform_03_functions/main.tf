a376b334a0c8355cd17e7bc5fa8206fe9062ab7e
mgallegoa <manuelgallego2003@yahoo.com>
2025-09-05 11:04:54 -0500

    I-as-C : TERRAFORM : Example 03 to use functions to create 3 ec2 instance and 3 cloudWatch logs.

---
+++ terraform_03_functions/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+# Other way to define variables. To define variables in local file.
+locals {
+  instanse_tag = "Tag name from variable in locals"
+}
+
+resource "aws_instance" "basic_example" {
+  for_each      = var.ec2_instances_name
+  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t3.micro"
+  tags = {
+    ExtraTag = local.instanse_tag
+    name     = "EC2_${each.key}"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "ec2_instances_log_group" {
+  for_each = var.ec2_instances_name
+  tags = {
+    Environment = "test"
+    Service     = each.key
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
